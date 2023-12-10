22cb8a7b1004b0e358b40807d53e7149898de939
edsonalcala <edson_alcala@msn.com>
2023-12-10 16:44:16 +0100

    config and deps

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,5 +1,12 @@
+require('dotenv').config()
+
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+import "hardhat-deploy"
+
+if (process.env.MASTER_MNEMONIC === undefined) {
+  console.warn("MASTER_MNEMONIC is not set")
+}
 
 const config: HardhatUserConfig = {
   solidity: {
@@ -7,10 +14,27 @@ const config: HardhatUserConfig = {
     settings: {
       optimizer: {
         enabled: true,
-        runs: 10000
+        runs: 10000,
+      },
+    }
+  },
+  namedAccounts: {
+    deployer: 0
+  },
+  networks: {
+    hardhat: {
+      accounts: {
+        mnemonic: process.env.MASTER_MNEMONIC || "",
+        count: 5
+      }
+    },
+    base_goerli: {
+      url: `https://base-goerli.g.alchemy.com/v2/${process.env.ALCHEMY_API_KEY || ""}`,
+      accounts: {
+        mnemonic: process.env.DEPLOYER_MNEMONIC || ""
       }
     }
   }
-};
+}
 
 export default config;
